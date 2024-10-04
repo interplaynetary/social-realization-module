eb13d3ec7087b98fc3303eeaf9789a4c914d4e60
dan <danielniemann@protonmail.com>
2024-10-04 22:33:48 +0200

    close MODAL on ESC key

--- frontend/src/components/Overlay/Overlay.tsx
+++ frontend/src/components/Overlay/Overlay.tsx
@@ -9,11 +9,19 @@ type OverlayProps = {
 const Overlay: React.FC<OverlayProps> = ({ children, onClose }) => {
     useEffect(() => {
         document.body.classList.add("body-fixed");
+        document.addEventListener("keydown", handleKeyDown);
 
         return () => {
             document.body.classList.remove("body-fixed");
+            document.removeEventListener("keydown", handleKeyDown);
         };
     }, []);
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+        if (e.key === "Escape") {
+            onClose();
+        }
+    };
 
     return (
         <div className={styles.overlay} onClick={onClose}>
